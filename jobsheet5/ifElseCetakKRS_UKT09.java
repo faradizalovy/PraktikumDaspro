78bcfb08009ac1b4457057fcad19b3206a256deb
faradizalovy <lovyfaradiza@gmail.com>
2025-10-07 20:32:19 +0700

    Modifikasi Percobaan 1

---
+++ jobsheet5/ifElseCetakKRS_UKT09.java
@@ -0,0 +1,20 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class ifElseCetakKRS_UKT09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---Cetak KRS Siakad---");
+        System.out.println("Apakah UKT sudah lunas? (true/false):");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("KRS dapat dicetak");
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
+    }
+    sc.close();
+}
+    
+}
